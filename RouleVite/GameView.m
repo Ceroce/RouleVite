4f79d3c9cb5b40823f8fb9d6ccfc30557eb8059b
Renaud Pradenc <renaud@ceroce.com>
2012-07-03 09:53:18 +0200

    Dessin du fond, du sol et de la balle.

--- RouleVite/GameView.m
+++ RouleVite/GameView.m
@@ -8,6 +8,21 @@
 
 #import "GameView.h"
 
+#define GROUND_Y    272.0
+#define BALL_DIAMETER   32.0
+
+@interface GameView ()
+{
+    float ballElevation;
+}
+
+- (void) _drawBackground;
+- (void) _drawGround;
+- (void) _drawBall;
+
+@end
+
+
 @implementation GameView
 
 - (id)initWithFrame:(CGRect)frame
@@ -23,9 +38,29 @@
 
 - (void)drawRect:(CGRect)rect
 {
-    [[UIColor colorWithRed:118.0/255.0 green:181.0/255.0 blue:252.0/255.0 alpha:1.0] setFill];
-    UIRectFill(rect);
+    [self _drawBackground];
+    [self _drawGround];
+    [self _drawBall];
 }
 
+
+- (void) _drawBackground
+{
+    [[UIColor colorWithRed:118.0/255.0 green:181.0/255.0 blue:252.0/255.0 alpha:1.0] setFill];
+    UIRectFill(self.bounds);    
+}
+
+- (void) _drawGround
+{
+    [[UIColor colorWithRed:125.0/255.0 green:70.0/255.0 blue:25.0/255.0 alpha:1.0] setFill];
+    UIRectFill(CGRectMake(0.0, GROUND_Y, 480.0, 48.0));      
+}
+
+- (void) _drawBall
+{
+    [[UIColor redColor] setFill];
+    CGRect ballRect = CGRectMake(50.0, GROUND_Y-BALL_DIAMETER, BALL_DIAMETER, BALL_DIAMETER);
+    [[UIBezierPath bezierPathWithOvalInRect:ballRect] fill];
+}
 
 @end
